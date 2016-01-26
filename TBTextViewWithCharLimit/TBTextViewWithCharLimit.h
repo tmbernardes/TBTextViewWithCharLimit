21ee7e29f3d28c082d87678e7f65fc9dd07f92ae
thiagotmb <tmb0710@gmail.com>
2016-01-26 16:22:48 -0200

    Initial Commit

---
+++ TBTextViewWithCharLimit/TBTextViewWithCharLimit.h
@@ -0,0 +1,19 @@
+//
+//  TBTextViewWithCharLimit.h
+//  TBTextViewWithCharLimit
+//
+//  Created by Thiago-Bernardes on 1/26/16.
+//  Copyright © 2016 TMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TBTextViewWithCharLimit.
+FOUNDATION_EXPORT double TBTextViewWithCharLimitVersionNumber;
+
+//! Project version string for TBTextViewWithCharLimit.
+FOUNDATION_EXPORT const unsigned char TBTextViewWithCharLimitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TBTextViewWithCharLimit/PublicHeader.h>
+
+
